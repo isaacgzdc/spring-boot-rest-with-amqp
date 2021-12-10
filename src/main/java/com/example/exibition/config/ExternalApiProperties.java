d72656b20fe94e9de7e5fca4e5417c00befa0b0c
isaacgzdc <isaacgzdc@gmail.com>
2021-12-10 18:53:38 +0100

    remove post-construct from properties class

--- src/main/java/com/example/exibition/config/ExternalApiProperties.java
+++ src/main/java/com/example/exibition/config/ExternalApiProperties.java
@@ -1,15 +1,10 @@
 package com.example.exibition.config;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Configuration
 @ConfigurationProperties(prefix = "external")
 @PropertySource(value = {"classpath:external.yml"})
@@ -20,10 +15,4 @@ public class ExternalApiProperties {
 	@Value( "${port}" )
 	private String port;
 	
-	@PostConstruct
-	public void log() {
-		log.debug("============================================================");
-		log.debug("{} {} {}",host,port);
-		log.debug("============================================================");
-	}
 }
